496e44aaee6c9c8bf84276fff375f9bf610c5067
Panagiotis Sakagiannis <bagjohn0@gmail.com>
2022-07-02 19:21:41 +0100

    tests revisited

---
+++ tests/view/replays/replay.py
@@ -0,0 +1,10 @@
+from lib.conf.stored.conf import loadRef, kConfDict
+
+# print(kConfDict('Ref'))
+# raise
+# refID = 'None.Tane_test'
+refID = 'exploration.3controls'
+d = loadRef(refID)
+d.visualize(s0=None, e0=None, vis_kwargs=None, agent_ids=None, save_to=None, time_range=None,
+            draw_Nsegs=None, env_params=None, track_point=None, dynamic_color=None, use_background=False,
+            transposition=None, fix_point=None, fix_segment=None)
