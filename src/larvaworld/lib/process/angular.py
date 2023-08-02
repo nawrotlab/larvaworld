d0cf9c3b18b46d20ab7d82e3e028a84c7d18f75f
Panagiotis Sakagiannis <bagjohn0@gmail.com>
2023-08-02 06:51:57 +0300

    configuration classes parameterized

--- src/larvaworld/lib/process/angular.py
+++ src/larvaworld/lib/process/angular.py
@@ -125,8 +125,7 @@ def angular_processing(s, e, c, d=None, recompute=False, mode='minimal', **kwarg
 
             s[fo]=  aux.apply_per_level(s[['x', 'y']], func).flatten()
         else :
-            xy_pars = c.midline_xy
-            Axy = s[xy_pars].values
+            Axy = s[c.midline_xy].values
             Ax, Ay = Axy[:, ::2], Axy[:, 1::2]
             Adx = np.diff(Ax)
             Ady = np.diff(Ay)
