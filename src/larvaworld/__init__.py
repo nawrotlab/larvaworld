4bd1d38fbcdcc5cc89268ac7678098ce0eb661a7
Panagiotis Sakagiannis <bagjohn0@gmail.com>
2023-09-11 19:23:23 +0300

    step-wise module import to facilitate automatic documentation

--- src/larvaworld/__init__.py
+++ src/larvaworld/__init__.py
@@ -1,6 +1,6 @@
 #from .lib import aux, param
 #from .lib import reg, plot, model, process, screen, sim, util
-from . import lib, cli
+from . import lib, cli, gui
 # # from .cli import SimModeParser
 # from .lib.process.dataset import BaseLarvaDataset,LarvaDataset, LarvaDatasetCollection
 # from .lib.reg import graphs
--- src/larvaworld/gui/__init__.py
+++ src/larvaworld/gui/__init__.py
@@ -1,1 +1,2 @@
+from . import gui_aux, tabs, panel
 from .panel.track_viewer import TrackViewer
