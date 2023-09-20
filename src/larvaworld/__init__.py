46ba81674cc2510db10eb7d52cfaf6e746ad7d5a
Panagiotis Sakagiannis <bagjohn0@gmail.com>
2023-09-20 18:25:53 +0300

    added docs folder

--- src/larvaworld/__init__.py
+++ src/larvaworld/__init__.py
@@ -1,12 +1,6 @@
-#from .lib import aux, param
-#from .lib import reg, plot, model, process, screen, sim, util
+
 from . import lib, cli, gui
-# # from .cli import SimModeParser
-# from .lib.process.dataset import BaseLarvaDataset,LarvaDataset, LarvaDatasetCollection
-# from .lib.reg import graphs
-#
-# # from .cli.argparser import SimModeParser
-#
+
 lib.reg.resetConfs(init=True)
 
 
