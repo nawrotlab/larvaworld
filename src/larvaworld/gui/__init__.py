879ea4638a979c85cabba8af6b625bc9f18d3f25
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-09-17 15:56:28 +0200

    used underscore to remove masked import to external panel library

--- src/larvaworld/gui/__init__.py
+++ src/larvaworld/gui/__init__.py
@@ -2,7 +2,7 @@
 The larvaworld Graphical User Interface (GUI)
 """
 
-from . import gui_aux, tabs, panel
-from .panel.track_viewer import TrackViewer
+from . import gui_aux, tabs, panel_
+from .panel_.track_viewer import TrackViewer
 
 __displayname__ = 'GUI'
