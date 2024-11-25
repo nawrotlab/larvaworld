b660ef0b71e67149aa218b33f9aa3c9eb53689f4
Panagiotis Sakagiannis <bagjohn0@gmail.com>
2024-11-25 10:50:59 +0200

    chore : argparser for Eval runs

--- src/larvaworld/cli/argparser.py
+++ src/larvaworld/cli/argparser.py
@@ -155,16 +155,21 @@ def parser_entry_from_param(k, p):
         d.action = "store_true" if not v else "store_false"
     elif c == param.String:
         d.type = str
-    elif c in param.Integer.__subclasses__():
+    elif c in [param.Integer] + param.Integer.__subclasses__():
         d.type = int
-    elif c in param.Number.__subclasses__():
+    elif c in [param.Number] + param.Number.__subclasses__():
         d.type = float
-    elif c in param.Tuple.__subclasses__():
+    elif c in [param.Tuple] + param.Tuple.__subclasses__():
         d.type = tuple
 
     if hasattr(p, "objects"):
         d.choices = p.objects
-        if c in param.List.__subclasses__():
+        if (
+            c
+            in [param.List, param.ListSelector]
+            + param.List.__subclasses__()
+            + param.ListSelector.__subclasses__()
+        ):
             d.nargs = "+"
         if hasattr(p, "item_type"):
             d.type = p.item_type
@@ -382,15 +387,12 @@ def parser_dict_from_dict(d0):
     return p.flatten()
 
 
-class SimModeParser:
+class SimModeParser(ArgumentParser):
     """
     Parser for simulation modes and arguments.
     """
 
     def __init__(self):
-        """
-        Initialize parsers for different simulation modes.
-        """
         # self.dict = AttrDict(
         #     {
         #         "Batch": [],
@@ -402,47 +404,52 @@ class SimModeParser:
         # )
         # ks = aux.SuperList(self.dict.values()).flatten.unique
         # self.parsers = aux.AttrDict({k: ParserArgumentDict.from_dict(reg.par.PI[k]) for k in ks})
-        self.parsers = AttrDict()
-        self.parsers.screen_kws = ParserArgumentDict.from_param(d0=ScreenOps)
-        self.parsers.SimOps = ParserArgumentDict.from_param(d0=SimOps)
-        self.parsers.RuntimeOps = ParserArgumentDict.from_param(d0=RuntimeOps)
-
-        # TODO set the parsers per mode
-        self.parsers.Replay = ParserArgumentDict.from_param(d0=reg.gen.Replay)
-        self.parsers.Eval = ParserArgumentDict.from_param(d0=reg.gen.Eval)
-        self.parsers.GAselector = ParserArgumentDict.from_param(d0=reg.gen.GAselector)
-        self.parsers.GAevaluation = ParserArgumentDict.from_param(
-            d0=reg.gen.GAevaluation
-        )
-
-        self.cli_parser = self.build_cli_parser()
         self.mode = None
         self.run = None
         self.args = AttrDict()
         self.run_kws = AttrDict()
+        self.parser_dicts = AttrDict(
+            {
+                "screen_kws": ParserArgumentDict.from_param(d0=ScreenOps),
+                "SimOps": ParserArgumentDict.from_param(d0=SimOps),
+                "RuntimeOps": ParserArgumentDict.from_param(d0=RuntimeOps),
+                "Replay": ParserArgumentDict.from_param(d0=reg.gen.Replay),
+                "Eval": ParserArgumentDict.from_param(d0=reg.gen.Eval),
+                "GAselector": ParserArgumentDict.from_param(d0=reg.gen.GAselector),
+                "GAevaluation": ParserArgumentDict.from_param(d0=reg.gen.GAevaluation),
+            }
+        )
+        super().__init__(
+            prog="larvaworld", description="CLI for running larvaworld simulations"
+        )
+        self.add_argument(
+            "-verbose",
+            "--VERBOSE",
+            type=int,
+            default=2,
+            help="Level of verbosity in the output",
+        )
+        self.add_argument(
+            "-parsargs",
+            "--show_parser_args",
+            action="store_true",
+            default=False,
+            help="Whether to show the parser argument namespace",
+        )
+        subparsers = self.add_subparsers(
+            dest="sim_mode",
+            help="The simulation mode to launch",
+            parser_class=ArgumentParser,
+        )
+        for m in SIMTYPES:
+            sp = subparsers.add_parser(m)
+            sp = self.init_mode_subparser(sp, m)
 
     def parse_args(self):
         """
         Parse command line arguments.
         """
-        self.args = AttrDict(vars(self.cli_parser.parse_args()))
-        print(self.args)
-
-    # def populate_mode_subparser(self, sp, m):
-    #     """
-    #     Populate a subparser with arguments for a specific simulation mode.
-
-    #     :param sp: The subparser.
-    #     :param m: The simulation mode.
-    #     :return: The modified subparser.
-    #     """
-    #     sp = self.parsers.screen_kws.add(sp)
-    #     sp = self.parsers.RuntimeOps.add(sp)
-    #     if m not in ["Replay", "Eval"]:
-    #         sp = self.parsers.SimOps.add(sp)
-    #     # for k in self.dict[m]:
-    #     #     sp = self.parsers[k].add(sp)
-    #     return sp
+        self.args = AttrDict(vars(super().parse_args()))
 
     def init_mode_subparser(self, sp, m):
         """
@@ -457,19 +464,7 @@ class SimModeParser:
             sp.add_argument(
                 "experiment", choices=reg.conf[m].confIDs, help="The experiment mode"
             )
-        # if m in ["Eval", "Replay"]:
-        #     sp.add_argument(
-        #         "-refID",
-        #         "--refID",
-        #         choices=reg.conf.Ref.confIDs,
-        #         help="The reference ID under which the dataset is stored",
-        #     )
-        #     sp.add_argument(
-        #         "-dir",
-        #         "--dir",
-        #         type=str,
-        #         help="The directory containing the dataset to be replayed"
-        #     )
+            sp = self.parser_dicts.SimOps.add(sp)
         if m in ["Exp", "Batch"]:
             sp.add_argument(
                 "-N",
@@ -507,43 +502,16 @@ class SimModeParser:
                 help="Whether to show the plots generated during data-analysis",
             )
 
-        sp = self.parsers.screen_kws.add(sp)
-        sp = self.parsers.RuntimeOps.add(sp)
-        if m not in ["Replay", "Eval"]:
-            sp = self.parsers.SimOps.add(sp)
-        # if m == "Replay":
-        # for k in self.dict[m]:
-        #     sp = self.parsers[k].add(sp)
-
+        sp = self.parser_dicts.screen_kws.add(sp)
+        sp = self.parser_dicts.RuntimeOps.add(sp)
+        if m == "Replay":
+            sp = self.parser_dicts.Replay.add(sp)
+        elif m == "Eval":
+            sp = self.parser_dicts.Eval.add(sp)
+        elif m == "Ga":
+            sp = self.parser_dicts.GAselector.add(sp)
+            sp = self.parser_dicts.GAevaluation.add(sp)
         return sp
-
-    def build_cli_parser(self):
-        """
-        Build the command line argument parser.
-        """
-        p = ArgumentParser()
-        p.add_argument(
-            "-verbose",
-            "--VERBOSE",
-            type=int,
-            default=2,
-            help="Level of verbosity in the output",
-        )
-        p.add_argument(
-            "-parsargs",
-            "--show_parser_args",
-            action="store_true",
-            default=False,
-            help="Whether to show the parser argument namespace",
-        )
-        subparsers = p.add_subparsers(
-            dest="sim_mode", help="The simulation mode to launch"
-        )
-        for m in SIMTYPES:
-            sp = subparsers.add_parser(m)
-            sp = self.init_mode_subparser(sp, m)
-            # sp = self.populate_mode_subparser(sp, m)
-        return p
 
     def eval_parser(self, p_key):
         """
@@ -552,7 +520,7 @@ class SimModeParser:
         :param p_key: The argument key.
         :return: The parsed value of the argument.
         """
-        return self.parsers[p_key].get(self.args)
+        return self.parser_dicts[p_key].get(self.args)
 
     def configure(self, show_args: bool = False):
         """
@@ -649,7 +617,6 @@ class SimModeParser:
         """
         Launch the simulation run.
         """
-        anal_kws = AttrDict({"show": self.args.show})
         m = self.mode
         r = self.run
         if m == "Batch":
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,28 +5,20 @@ cli_entry_point = "larvaworld"
 
 def test_run_experiment_no_analysis_no_video():
     """Run an experiment without analysis, without visualization on screen."""
-    result = shell(f"{cli_entry_point} Exp dish -N 10 -duration 3.0")
+    result = shell(f"{cli_entry_point} Exp dish -N 4 -duration 1.0")
     assert result.exit_code == 0
 
 
 def test_run_experiment_with_analysis_no_video():
     """Run an experiment with analysis, without visualization on screen."""
-    result = shell(f"{cli_entry_point} Exp chemorbit -N 10 -duration 3.0 -a")
+    result = shell(f"{cli_entry_point} Exp chemorbit -N 4 -duration 1.0 -a")
     assert result.exit_code == 0
 
 
 def test_run_experiment_with_analysis_with_video():
     """Run an experiment with analysis, with visualization on screen."""
     result = shell(
-        f"{cli_entry_point} Exp dispersion -N 10 -duration 3.0 -vis_mode video -a"
-    )
-    assert result.exit_code == 0
-
-
-def test_run_experiment_no_analysis_with_video():
-    """Run an experiment without analysis, with visualization on screen."""
-    result = shell(
-        f"{cli_entry_point} Exp dispersion -N 10 -duration 3.0 -vis_mode video"
+        f"{cli_entry_point} Exp dispersion -N 4 -duration 2.0 -vis_mode video -a"
     )
     assert result.exit_code == 0
 
@@ -34,34 +26,34 @@ def test_run_experiment_no_analysis_with_video():
 def test_run_experiment_no_analysis_with_image():
     """Run an experiment without analysis, with visualization on screen."""
     result = shell(
-        f"{cli_entry_point} Exp dispersion -N 10 -duration 3.0 -vis_mode image"
+        f"{cli_entry_point} Exp dish -N 4 -duration 2.0 -vis_mode image"
     )
     assert result.exit_code == 0
 
 
-def xtest_run_ga_without_video():
+def test_run_ga_without_video():
     """Run a genetic algorithm experiment without visualization on screen."""
     result = shell(
-        f"{cli_entry_point} Ga realism -refID exploration.30controls -N 20 -duration 0.5 -mID1 GA_test_loco"
+        f"{cli_entry_point} Ga realism -refID exploration.30controls -Nagents 10 -Ngenerations 5 -duration 0.5 -bestConfID GA_test_loco -init_mode model"
     )
     assert result.exit_code == 0
 
 
-def xtest_run_ga_with_video():
+def test_run_ga_with_video():
     """Run a genetic algorithm experiment with visualization on screen."""
     result = shell(
-        f"{cli_entry_point} Ga realism -refID exploration.30controls -N 20 -duration 0.5 -mID1 GA_test_loco -vis_mode image"
+        f"{cli_entry_point} Ga realism -refID exploration.30controls -Nagents 10 -Ngenerations 5 -duration 0.5 -bestConfID GA_test_loco -init_mode model -vis_mode image"
     )
     assert result.exit_code == 0
 
 
-def xtest_run_replay_on_dataset_by_ID():
+def test_run_replay_on_dataset_by_ID():
     """Run an experiment replay specifying the dataset by its reference ID."""
     result = shell(f"{cli_entry_point} Replay -refID exploration.30controls")
     assert result.exit_code == 0
 
 
-def xtest_run_replay_on_dataset_by_dir():
+def test_run_replay_on_dataset_by_dir():
     """Run an experiment replay specifying the dataset by its directory path."""
     result = shell(
         f"{cli_entry_point} Replay -refDir SchleyerGroup/processed/exploration/30controls"
@@ -75,10 +67,10 @@ def xtest_batch_run():
     assert result.exit_code == 0
 
 
-def xtest_evaluation_run():
+def test_evaluation_run():
     """Perform an experiment evaluation run."""
     result = shell(
-        f"{cli_entry_point} Eval -refID exploration.30controls -mIDs RE_NEU_PHI_DEF RE_SIN_PHI_DEF -N 5"
+        f"{cli_entry_point} Eval -refID exploration.30controls -modelIDs RE_NEU_PHI_DEF RE_SIN_PHI_DEF -N 10"
     )
     assert result.exit_code == 0
 
