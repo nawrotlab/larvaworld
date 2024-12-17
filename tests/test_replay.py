2aa422133d3f9a810ea7b342991683f0c679a47b
Panagiotis Sakagiannis <bagjohn0@gmail.com>
2024-12-17 15:54:26 +0200

    chore: test_replay parametrize

--- tests/test_replay.py
+++ tests/test_replay.py
@@ -5,59 +5,55 @@ from larvaworld.lib import reg, sim, util
 larvaworld.VERBOSE = 1
 
 
-
-
-# @pytest.mark.parametrize("a", range(1000))
-def test_replay():
-    refID = reg.default_refID
-    d = reg.conf.Ref.loadRef(refID)
-    replay_kws = {
-        "normal": {"time_range": (10, 80)},
-        "dispersal": {"transposition": "origin", "time_range": (60, 120)},
-        "fixed_point": {
+replay_confs = [
+    ("normal", {"time_range": (10, 80)}),
+    ("dispersal", {"transposition": "origin", "time_range": (60, 120)}),
+    (
+        "fixed_point",
+        {
             "agent_ids": [0],
             "close_view": True,
             "fix_point": 6,
             "time_range": (80, 100),
         },
-        "fixed_segment": {
+    ),
+    (
+        "fixed_segment",
+        {
             "agent_ids": [0],
             "close_view": True,
             "fix_point": 6,
             "fix_segment": "rear",
             "time_range": (100, 130),
         },
-        "fixed_overlap": {
+    ),
+    (
+        "fixed_overlap",
+        {
             "agent_ids": [0],
             "close_view": True,
             "fix_point": 6,
             "fix_segment": "front",
             "overlap_mode": True,
         },
-        "2segs": {"draw_Nsegs": 2},
-        "all_segs": {"draw_Nsegs": d.config.Npoints - 1},
-    }
+    ),
+    ("2segs", {"draw_Nsegs": 2}),
+    ("all_segs", {"draw_Nsegs": 11}),
+]
 
-    for mode, kws in replay_kws.items():
-        print(mode)
-        parameters = reg.gen.Replay(
-            **util.AttrDict(
-                {
-                    "refID": refID,
-                    # 'dataset' : dataset,
-                    **kws,
-                }
-            )
-        ).nestedConf
-        rep = sim.ReplayRun(
-            parameters=parameters,
-            dataset=d,
-            id=f"{refID}_replay_{mode}",
-            dir=f"./media/{mode}",
-        )
-        output = rep.run()
-        assert output.parameters.constants["id"] == rep.id
-        # raise
+
+@pytest.mark.parametrize("id,conf", replay_confs)
+def test_replay(id, conf):
+    refID = reg.default_refID
+    # d = reg.loadRef(refID, load=True)
+    rep = sim.ReplayRun(
+        parameters=reg.gen.Replay(refID=refID, **conf).nestedConf,
+        # dataset=d,
+        id=f"{refID}_replay_{id}",
+        dir=f"./media/{id}",
+    )
+    output = rep.run()
+    assert output.parameters.constants["id"] == rep.id
 
 
 def test_replay_by_dir():
